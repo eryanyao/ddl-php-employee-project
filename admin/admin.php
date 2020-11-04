ba7d16a6aaa323134e8a4c95276bda8b5d234727
YY <yunais143@gmail.com>
2020-11-04 22:54:32 +0800

    1

--- admin/admin.php
+++ admin/admin.php
@@ -66,8 +66,8 @@ table, thead, tr, th, td {
         <th class="content">'.$row['idAdmin'].'</th>
         <td class="content">'.$row['uidAdmin'].'</td>
         <td class="content">'.$row['emailAdmin'].'</td>
-        <td class="mb-2"><input type="button" style="width:100px;" onclick="window.location = \'edit.php?sn='.$row['idAdmin'].'\'" value="Edit" class="btn btn-success"></td>
-        <td><input type="button" style="width:100px;"onclick="window.location = \'delete.php?sn='.$row['idAdmin'].'\'" value="Delete" class="btn btn-danger"></td></tr></tbody></div>
+        <td class="mb-2"><input type="button" style="width:100px;" onclick="window.location = \'admin.php?sn='.$row['idAdmin'].'\'" value="Edit" class="btn btn-success"></td>
+        <td><input type="button" style="width:100px;"onclick="window.location = \'admin.php?delete='.$row['idAdmin'].'\'" value="Delete" class="btn btn-danger"></td></tr></tbody></div>
         </tr>';
       };
       echo '</tbody>
@@ -75,6 +75,22 @@ table, thead, tr, th, td {
       </div><br>';
 
   ?>
+
+<?php 
+    if(isset($_GET['delete'])){
+      ?>
+       <script>
+        swal({
+          title: "Data Deleted",
+          text: "Your admin information has been deleted!",
+          icon: "success",
+          button: "Yes"
+          
+        }).then(function(){
+          window.location.href = "admin.php";
+        });
+        </script>
+       <?php $delete = mysqli_query($conn, "DELETE FROM admin WHERE idAdmin='{$_GET['delete']}'"); }?>
 
   </div>
 
